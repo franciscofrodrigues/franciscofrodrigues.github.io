d1b3d580dd9c1198101bb29daea2540ec728e5c0
f_rodrigues <franciscofrodrigues28@gmail.com>
2023-11-24 13:23:19 +0000

    Script changes

--- js/example.js
+++ js/example.js
@@ -1,64 +1,94 @@
 let txt =
   "O meu nome é Francisco Rodrigues tenho 21 anos, sou licenciado em Design e Multimédia";
 
-let bg = "#000000";
-let fg = "#f1f1f1";
+let img = [];
+
+let white = "#f1f1f1";
 
 let font;
 
+let lockPosition = 0;
+
 function preload() {
   font = loadFont("assets/fonts/InknutAntiqua-Regular.ttf");
+  img[0] = loadImage("assets/images/pai.jpg");
+  img[1] = loadImage("assets/images/eu.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
+  imageMode(CENTER);
   rectMode(CENTER, CENTER);
   textFont(font);
   textAlign(LEFT, CENTER);
   textSize(12);
   textWrap(WORD);
 
-  
+  let res = 200;
+  img[0].resize(res, res);
+  img[1].resize(res, res);
 }
 
 function draw() {
   clear();
-  // orbitControl();
 
-  //Camera e Prespectiva
-  // perspective(PI / 4.0, float(width / height), 1, 100000);
-  // camera(0, 0, height / 2 / tan(PI / 6), 0, 0, 0, 0, 1, 0);
-  
-  //TRANSFORMAÇÕES INICIAIS
-  rotateY(map(mouseX, 0, width, -PI, PI));
-  translate(-windowWidth/2, -windowHeight/2);
+  //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––CÂMERA E PRESPECTIVA
+  perspective(PI / 2.9, float(width / height), 1, 100000);
+  camera(0, 0, height / 2 / tan(PI / 6), 0, 0, 0, 0, 1, 0);
 
-  //GUIDES
-  //Linhas
-  stroke("#ff0000");
-  line(0, height/2, width, height/2);
-  line(width/2, 0, width/2, height);
+  // let wave = map(sin(radians(frameCount)), -1, 1, -22, 22);
+  // let wave = map(mouseX, 0, width, -22, 22);
+  // rotateY(radians(wave));
 
-  //Rectângulo Central
-  push();
-  fill("#ff0000");
-  noStroke();
-  rect(width/2, height/2, 10, 10);
-  pop();
+  //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––TRANSFORMAÇÕES INICIAIS
 
+  lockPosition = map(mouseY, 0, height, -200, 600);
 
-  //DESCRIÇÃO SOBRE MIM
+  translate(
+    -windowWidth / 2,
+    -windowHeight / 2,
+    lockPosition
+  );
+
+  //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––GUIDES
+  // //Linhas
+  // stroke("#ff0000");
+  // line(0, height / 2, width, height / 2);
+  // line(width / 2, 0, width / 2, height);
+
+  // //Rectângulo Central
+  // push();
+  // fill("#ff0000");
+  // noStroke();
+  // rect(width / 2, height / 2, 10, 10);
+  // pop();
+
+  //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––IMAGENS
+  for (let i = 0; i < img.length; i++) {
+    push();
+    translate(width / 2 - 200*i, height / 2, i * 200);
+    // rotateY(map(sin(radians(frameCount)), -1, 1, -22, 22));
+    image(img[i], 0, 0);
+    pop();
+  }
+
+  //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––DESCRIÇÃO SOBRE MIM
   push();
   //Propriedades Texto
-  fill(fg);
+  fill(white);
+
+  translate(width / 4, 0, 200);
 
   //Texto
-  text(txt, width/2, height/2, 200);
+  text(txt, width / 2, height / 2, 200);
   pop();
-  
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function mousePressed(){
+  lockPosition = 0;
 }
