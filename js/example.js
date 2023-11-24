80a4a082437eb2d30e0848ebbe4950d7a9586ec6
f_rodrigues <franciscofrodrigues28@gmail.com>
2023-11-24 15:27:21 +0000

    3d Gallery

--- js/example.js
+++ js/example.js
@@ -1,15 +1,17 @@
 let txt =
   "O meu nome é Francisco Rodrigues tenho 21 anos, sou licenciado em Design e Multimédia";
 
-let img = [];
+let img;
 
 let white = "#f1f1f1";
 
 let font;
 
+let lockPosition = -1000;
+
 function preload() {
   font = loadFont("assets/fonts/InknutAntiqua-Regular.ttf");
-  img[0] = loadImage("assets/images/eu.jpg");
+  img = loadImage("assets/images/eu-pequena.jpeg");
   // img[1] = loadImage("assets/images/eu.jpg");
 }
 
@@ -23,8 +25,10 @@ function setup() {
   textSize(12);
   textWrap(WORD);
 
-  let res = 200;
-  img[0].resize(res, res);
+  let res = 500;
+  img.resize(res, 0);
+  // img[0].resize(res, res);
+  // img[0].resize(res, 0);
 }
 
 function draw() {
@@ -40,13 +44,7 @@ function draw() {
 
   //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––TRANSFORMAÇÕES INICIAIS
 
-  lockPosition = map(mouseY, 0, height, -200, 600);
-
-  translate(
-    -windowWidth / 2,
-    -windowHeight / 2,
-    lockPosition
-  );
+  translate(-windowWidth / 2, -windowHeight / 2, lockPosition);
 
   //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––GUIDES
   // //Linhas
@@ -62,26 +60,31 @@ function draw() {
   // pop();
 
   //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––IMAGENS
-  for (let i = 0; i < img.length; i++) {
-    push();
-    translate(width / 2 - 200*i, height / 2, i * 200);
-    // rotateY(map(sin(radians(frameCount)), -1, 1, -22, 22));
-    image(img[i], 0, 0);
-    pop();
-  }
+  push();
+  translate(width/2 - width/6, height/2, 200);
+  rotateY(PI / 3);
+  image(img, 0, 0);
+  pop();
 
   //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––DESCRIÇÃO SOBRE MIM
   push();
   //Propriedades Texto
   fill(white);
 
-  translate(width / 4, 0, 200);
+  translate(width / 2 + width/20, height/2, 1000);
+  rotateY(-PI / 3);
 
   //Texto
-  text(txt, width / 2, height / 2, 200);
+  text(txt, 0, 0, 200);
   pop();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseWheel(event) {
+  // Use the event.delta to control the scroll behavior
+  lockPosition += event.delta * 0.7; // Adjust the speed here
+  return false; // Prevent the page from scrolling
 }
